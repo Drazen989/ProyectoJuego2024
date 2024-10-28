b2da321742c8a3e778e25b608c697122140d48db
Valitroxx <167727749+Valitroxx@users.noreply.github.com>
2024-10-28 16:42:03 -0300

    lkamsdlkaslkdaslkd

--- lwjgl3/src/main/java/puppy/code/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/puppy/code/lwjgl3/Lwjgl3Launcher.java
@@ -3,7 +3,6 @@ package puppy.code.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import puppy.code.BlockBreakerGame;
-
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
