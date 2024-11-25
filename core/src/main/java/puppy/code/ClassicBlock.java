b3af16b860d830e22bf280efe5f5670e17463a1a
fefex <ff.diaz.m17@gmail.com>
2024-11-24 23:52:30 -0300

    colores ramdon

--- core/src/main/java/puppy/code/ClassicBlock.java
+++ core/src/main/java/puppy/code/ClassicBlock.java
@@ -3,6 +3,7 @@ package puppy.code;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import java.util.Random; // Importamos la clase Random para generar números aleatorios
 
 public class ClassicBlock extends GameObject implements IBlock {
     private Color color;
@@ -11,7 +12,15 @@ public class ClassicBlock extends GameObject implements IBlock {
     public ClassicBlock(int x, int y, int width, int height) {
         super(x, y, width, height);
         this.destroyed = false;
-        this.color = Color.BLUE; // Color específico para el tema clásico
+
+        // Generamos un color aleatorio para cada bloque
+        Random r = new Random(x + y); // Semilla basada en la posición para obtener variedad
+        this.color = new Color(
+            0.1f + r.nextFloat() * 0.9f, // Componente rojo (R) entre 0.1 y 1.0
+            0.1f + r.nextFloat() * 0.9f, // Componente verde (G) entre 0.1 y 1.0
+            0.1f + r.nextFloat() * 0.9f, // Componente azul (B) entre 0.1 y 1.0
+            1f                              // Opacidad (A) al máximo
+        );
     }
 
     @Override
